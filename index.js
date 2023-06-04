44575abc4abd554ed3dd5d1bb750ffb00902d12e
Alan Juarez <alanwlfx@gmail.com>
2023-06-03 23:09:50 -0600

    Introduccion

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+   res.send('Hola mi primer server con express!')
+})
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola soy nueva ruta!')
+})
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Producto 1',
+    price: 1000
+  }
+  )
+})
+
+app.listen(port, () => {
+  console.log(`Mi port: ${port}`)
+})
