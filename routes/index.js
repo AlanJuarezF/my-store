9e367cfe46683e5a8adcd2a00bb77bea7ef80d17
Alan Juarez <ajf_451@hotmail.com>
2023-06-04 18:24:26 -0600

    metodo crear

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
+const express = require('express')
 const productsRouter = require('./ProductsRouter')
 const usersRouter = require('./UsersRouter')
 const categoriesRouter = require('./CategoriesRouter')
 
 function routerAPI(app) {
-app.use('/products', productsRouter);
-app.use('/users', usersRouter);
-app.use('/categories', categoriesRouter);
+  const router = express.Router()
+  app.use('/api/v1', router)
+router.use('/products', productsRouter);
+router.use('/users', usersRouter);
+router.use('/categories', categoriesRouter);
 }
 
 module.exports = routerAPI;
